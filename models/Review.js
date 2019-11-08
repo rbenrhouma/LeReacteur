9e125ef018a0586b991dab7c2b7cba9fb233346e
BEN RHOUMA Ridha <rbenrhouma2@gmail.com>
2019-11-08 19:38:22 +0100

    Update Review.js

--- models/Review.js
+++ models/Review.js
@@ -1,32 +1,26 @@
 const mongoose = require("mongoose");
-var Schema = mongoose.Schema;
 
-let schema = new Schema(
-  {
-    rating: {
-      type: Number,
-      min: 0,
-      max: 5,
-      required: true
-    },
-    comment: {
-      type: String,
-      minlength: 0,
-      maxlength: 150,
-      trim: true,
-      required: true
-    },
-    username: {
-      type: String,
-      minlength: 3,
-      maxlength: 15,
-      trim: true,
-      required: true
-    }
+const Review = mongoose.model("Review", {
+  rating: {
+    type: Number,
+    min: 0,
+    max: 5,
+    required: true
   },
-  { versionKey: false }
-);
-
-var Review = mongoose.model("Review", schema);
+  comment: {
+    type: String,
+    minlength: 0,
+    maxlength: 150,
+    trim: true,
+    required: true
+  },
+  username: {
+    type: String,
+    minlength: 3,
+    maxlength: 15,
+    trim: true,
+    required: true
+  }
+});
 
 module.exports = Review;
