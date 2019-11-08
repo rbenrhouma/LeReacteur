97de1ecd2cd132089b690d21b247d74f92ec85b4
BEN RHOUMA Ridha <rbenrhouma2@gmail.com>
2019-11-08 19:30:56 +0100

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,36 +1,34 @@
 const mongoose = require("mongoose");
-var Schema = mongoose.Schema;
 
-let schema = new Schema(
-  {
-    title: {
-      type: String,
-      default: ""
-    },
-    description: {
-      type: String,
-      default: ""
-    },
-    price: {
-      type: Number,
-      default: 0,
-      min: [0, "Should not be negative"]
-    },
-    category: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Category"
-    },
-    reviews: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Review"
-      }
-    ],
-    averageRating: { type: Number, min: 0, max: 5 }
+const Product = mongoose.model("Product", {
+  title: {
+    type: String,
+    default: ""
   },
-  { versionKey: false }
-);
-
-const Product = mongoose.model("Product", schema);
+  description: {
+    type: String,
+    default: ""
+  },
+  price: {
+    type: Number,
+    default: 0,
+    min: [0, "Should not be negative"]
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category"
+  },
+  reviews: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Review"
+    }
+  ],
+  averageRating: {
+    type: Number,
+    min: 0,
+    max: 5
+  }
+});
 
 module.exports = Product;
