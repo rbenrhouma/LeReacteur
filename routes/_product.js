4629e564e9b34818314fce03afd776871971b523
Ridha <rbenrhouma2@gmail.com>
2019-11-08 19:03:19 +0100

    copy product from Alex

--- routes/_product.js
+++ routes/_product.js
@@ -4,80 +4,112 @@ const router = express.Router();
 const Product = require("../models/Product");
 const Category = require("../models/Category");
 
+const createFilters = req => {
+  const filters = {};
+
+  if (req.query.priceMin) {
+    filters.price = {};
+    // $gte signifi qu'on va recuperer que les produits avec un "price" >= req.query.priceMin
+    filters.price.$gte = req.query.priceMin;
+  }
+  if (req.query.priceMax) {
+    // Condition pour pas RE-creer l'objet filters.price
+    if (filters.price === undefined) filters.price = {};
+    // $lte signifi qu'on va recuperer que les produits avec un "price" <= req.query.priceMax
+    filters.price.$lte = req.query.priceMax;
+  }
+  if (req.query.category) filters.category = req.query.category;
+  if (req.query.title) {
+    // Ici on crée une regular expression avec l'option "i" qui signifie insensitive
+    filters.title = new RegExp(req.query.title, "i");
+  }
+  return filters;
+};
+
 router.get("/", async (req, res) => {
-  console.log("yes products list");
   try {
-    var query = {};
-    if (req.query.priceMin || req.query.priceMax || req.query.category) query["$and"] = [];
+    const filters = createFilters(req); // On genere les filtres avec ce qu'il y a dans la query
 
-    var product;
-    //console.log(req.query);
-    if (req.query.priceMin) query["$and"].push({ price: { $gte: req.query.priceMin } });
-    if (req.query.priceMax) query["$and"].push({ price: { $lte: req.query.priceMax } });
-    if (req.query.category) query["$and"].push({ category: req.query.category });
+    // On lance une recherche avec les potentiels filtres sur les produits
+    const search = Product.find(filters)
+      .populate("category")
+      .populate("reviews");
 
-    // console.log(query);
-    if (query !== {}) {
-      product = await Product.find(query);
-    } else {
-      product = await Product.find();
-    }
-    if (req.query.sort === "price-asc") {
-      const search = product;
+    if (req.query.sort === "rating-asc") {
+      // ici on ajoute a notre recherche un tri
+      search.sort({ averageRating: 1 });
+    } else if (req.query.sort === "rating-desc") {
+      // ici on ajoute a notre recherche un tri
+      search.sort({ averageRating: -1 });
+    } else if (req.query.sort === "price-asc") {
+      // ici on ajoute a notre recherche un tri
       search.sort({ price: 1 });
-      const product = await search;
+    } else if (req.query.sort === "price-desc") {
+      // ici on ajoute a notre recherche un tri
+      search.sort({ price: -1 });
     }
 
-    res.json(product);
+    if (req.query.page) {
+      const page = req.query.page;
+      const limit = 2;
+      search.limit(limit).skip(limit * page);
+    }
+
+    // ici attend la fin de la recherche et on range tout dans products
+    const products = await search;
+
+    res.json(products);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 });
 
 router.post("/create", async (req, res) => {
+  const title = req.body.title;
+  const description = req.body.description;
+  const price = req.body.price;
+  const categoryId = req.body.category;
+
   try {
-    const title = req.body.title;
-    const description = req.body.description;
-    const price = req.body.price;
-    const categoryId = req.body.category;
-    const category = await Category.findById(categoryId);
-    if (title && category) {
-      const product = new Product({
-        title: title,
-        description: description,
-        price: price,
-        category: categoryId
-      });
-      await product.save();
-      res.json(product);
-    } else {
-      if (!category) res.status(400).json({ error: "Wrong category associated" });
-      else res.status(400).json({ error: "Wrong parameters" });
-    }
+    // On cree une categorie en remplissant le model
+    const product = new Product({
+      title: title,
+      description: description,
+      price: price,
+      category: categoryId
+    });
+    // On le sauvegarde pour le mettre dans le base de donnée
+    await product.save();
+    res.status(201).send("Product created");
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 });
 
 router.put("/update", async (req, res) => {
+  const productId = req.query.id;
+  const newTitle = req.body.title;
+  const newDescription = req.body.description;
+  const newPrice = req.body.price;
+  const newCategoryId = req.body.category;
+
   try {
-    const id = req.query.id;
-    const title = req.body.title;
-    const description = req.body.description;
-    const price = req.body.price;
+    // On cree une categorie en remplissant le model
+    const product = await Product.findById(productId);
 
-    if (id && title && description && price) {
-      const product = await Product.findById(id);
+    if (!product) return res.status(400).json({ error: "Product not found" });
 
-      product.title = title;
-      product.description = description;
-      product.price = price;
-
-      await product.save();
-      res.json(product);
-    } else {
-      res.status(400).json({ error: "Wrong parameters" });
+    if (newTitle) product.title = newTitle;
+    if (newDescription) product.description = newDescription;
+    if (newPrice) product.price = newPrice;
+    if (newCategoryId) {
+      const category = await Category.findById(newCategoryId);
+      product.category = category;
     }
+
+    // On le sauvegarde pour le mettre dans le base de donnée
+    await product.save();
+    res.status(200).json(product);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
@@ -85,11 +117,14 @@ router.put("/update", async (req, res) => {
 
 router.delete("/delete", async (req, res) => {
   try {
-    const id = req.query.id;
-    if (id) {
-      const product = await Product.findById(id);
-      await product.remove();
-      res.send("Product is delited");
+    const productId = req.query.id;
+    if (productId) {
+      const product = await Product.findById(productId); // Ici on recupere un product qui a comme id : req.body.id
+
+      if (product) await product.remove();
+      else return res.status(400).json({ error: "Product not found" });
+
+      res.send("Product deleted");
     } else {
       res.status(400).json({ error: "Wrong parameters" });
     }
