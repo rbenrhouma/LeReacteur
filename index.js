1dff1dbcb6bee64e2e5b96a883a3ab18af2e46dc
Ridha <rbenrhouma2@gmail.com>
2019-11-08 18:25:19 +0100

    afficher le nom de la base

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/product-catalog
   useNewUrlParser: true
 });
 
-console.log(process.env.MONGODB_URL);
+console.log("DataBase name :" + process.env.MONGODB_URL);
 // On recupere le router du fichier ./routes/index.js
 const routes = require("./routes/");
 
