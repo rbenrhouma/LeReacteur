28ea2e0f4ed59dad96f5c7ba1084648c2a3c12cd
BEN RHOUMA Ridha <rbenrhouma2@gmail.com>
2019-11-08 19:28:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ const mongoose = require("mongoose");
 const app = express();
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/product-catalog", {
-  useUnifiedTopology: true,
-  useNewUrlParser: true
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/product-catalog",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
 
 // On recupere le router du fichier ./routes/index.js
 const routes = require("./routes/");
@@ -16,12 +19,6 @@ const routes = require("./routes/");
 // On l'utilise
 app.use(routes);
 
-// si aucune page n'est disponible.
-app.all("*", (req, res) => {
-  res.status(404).send("Page introuvable dans products ...");
-});
-
 app.listen(process.env.PORT || 3000, () => {
-  console.log("DataBase name :" + process.env.MONGODB_URI);
-  console.log("Server démarré sur le port : " + process.env.PORT || 3000);
+  console.log("Server started trop cool");
 });
