bff254f46fd868d8ea5ea2bfe41d2d5331730b32
Ridha <rbenrhouma2@gmail.com>
2019-11-09 11:46:54 +0100

    si aucune page n'est disponible

--- index.js
+++ index.js
@@ -5,19 +5,21 @@ const mongoose = require("mongoose");
 const app = express();
 app.use(bodyParser.json());
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/product-catalog",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/product-catalog", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 // On recupere le router du fichier ./routes/index.js
 const routes = require("./routes/");
 
 // On l'utilise
 app.use(routes);
+
+// si aucune page n'est disponible.
+app.all("*", (req, res) => {
+  res.status(404).send("Page introuvable dans products ...");
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server started trop cool");
